fd2f35fb3087040b8e8f1044dffe0c7969fd1000
equipa6 <equip_a6@boscdelacoma.cat>
2022-05-17 23:16:29 +0200

    commit

--- clogin_server.py
+++ clogin_server.py
@@ -1,19 +1,22 @@
 import socket
 import threading
 
-ip = "172.21.233.33"
+ip = "127.0.0.1"
 port = 8432
 
 srv_clogin = socket.socket()
 srv_clogin.bind((ip, port))
-srv_clogin.listen(1000)
-clientes_list = []
+srv_clogin.listen(100)
+socket_id_clientes = []
+nombre_clientes = []
 
-def recibir_mensajes():
+def recibir_mensajes(conn, addr):
+    print("Ha entrat a la funcio")
     while True:
         try:
-            missatge = srv_clogin.recv(1024)
-            enviar_mensajes(missatge)
+            missatge = conn.recv(1024)
+            print("{} --> {}".format(missatge.decode(),addr[0]))
+            #enviar_mensajes(missatge)   
         except:
             pass
 
@@ -21,21 +24,15 @@ def enviar_mensajes(mss):
     mss = mss.decode()
     indice_coma = mss.index(",")
     nom_usuari = mss[0:indice_coma]
-    for i in clientes_list:
-        if i[0] == nom_usuari:
-            i[1].send(mss.encode())
-            break
-
-ejecutar_funcio = threading.Thread(recibir_mensajes)
-ejecutar_funcio.start()
 
 while True:
-    try:
-        connexio, address = srv_clogin.accept()
-        nom_client = connexio.recv(1024)
-        informacio_usuari = [nom_client.decode(), connexio]
-        clientes_list.append(informacio_usuari)
-        print("S'ha connectat l'usuari {} amb la següent IP --> {}".format(nom_client.decode(), address))
-        connexio.send("Correct".encode())
-    except:
-        pass
+    connexio, address = srv_clogin.accept()
+    nom_client = connexio.recv(1024)
+    socket_id_clientes.append(connexio)
+    nombre_clientes.append(nom_client.decode())
+    print("S'ha connectat l'usuari {} amb la IP --> {}".format(nom_client.decode(), address))
+    connexio.send("Correct".encode())  
+    ejecutar_funcio = threading.Thread(target=recibir_mensajes, args=(connexio, address))
+    ejecutar_funcio.daemon = True
+    ejecutar_funcio.start()
+        
